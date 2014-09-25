cf56fc8b6e5a2ff89644ca416431762bc2205cb9
aleaf <andrew.t.leaf@gmail.com>
2014-09-25 17:47:36 -0500

    rewrote writing of tsf file to fix node numbering issue

--- gridspec.py
+++ gridspec.py
@@ -1,64 +1,143 @@
 __author__ = 'aleaf'
 
-import pandas as pd
 import numpy as np
-from shapely.geometry import box
+
 
 tsf = '/Users/aleaf/Documents/ATLData/Wbasin/grid02qtg.tsf'
 nod_file = '/Users/aleaf/Documents/ATLData/Wbasin/output_usgdata/grid02qtg.nod'
 
-class gridSPC:
 
-    def __init__(self, nod_file):
-        self.nod_file = nod_file
-        self.outfile = nod_file[:-4] + '.spc'
-
-    def read_nod(self):
-
-        self.nods = pd.read_csv(self.nod_file, skiprows=1,
-                           delim_whitespace=True,
-                           names=['node', 'layer', 'centroid_x', 'centroid_y', 'centroid_z', 'delta_x', 'delta_y', 'delta_z'],
-                           header=None)
-
-        self.nnode = len(self.nods)
-        self.nlay = len(np.unique(self.nods['layer']))
-        self.iz = 1
-        self.ic = 1
+class parentGrid(object):
 
 
-        self.nvertex = self.nnode * 8 # for now, write all eight vertices for each node
+    def __init__(self, nrow, ncol, nlay, xll=0, yll=0, dxy=1):
 
-        # build a list of vertices for each node
-
-        def vertices(x):
-            xmin, xmax = x['centroid_x'] - 0.5 * x['delta_x'], x['centroid_x'] + 0.5 * x['delta_x']
-            ymin, ymax = x['centroid_y'] - 0.5 * x['delta_y'], x['centroid_y'] + 0.5 * x['delta_y']
-            zmin, zmax = x['centroid_z'] - 0.5 * x['delta_z'], x['centroid_z'] + 0.5 * x['delta_z']
-
-            x['verts'] = [(xmin, ymin, zmin),
-                          (xmin, ymax, zmin),
-                          (xmax, ymin, zmin),
-                          (xmax, ymax, zmin),
-                          (xmin, ymin, zmax),
-                          (xmin, ymax, zmax),
-                          (xmax, ymin, zmax),
-                          (xmax, ymax, zmax)]
+        self.nrow = nrow
+        self.ncol = ncol
+        self.nlay = nlay
+        self.xll = xll
+        self.yll = yll
+        self.dxy = dxy
 
 
-        self.nods.apply(vertices, axis=1)
-        #verts = [vertices(r) for i, r in nods.iterrows()]
+    def read_elevations(self, topfiles, botfiles):
+
+        print "reading cell top and bottom elevations..."
+        self.tops = np.array([], dtype=float)
+        for i, f in enumerate(sorted(topfiles)):
+            print f
+            self.tops = np.append(self.tops, np.fromfile(f, sep=' '))
+
+        self.bots = np.array([], dtype=float)
+        for i, f in enumerate(sorted(botfiles)):
+            print f
+            self.bots = np.append(self.bots, np.fromfile(f, sep=' '))
+
+        self.nnod = len(self.bots)
 
 
-    def write_byline(self, nlay, iz=1, ic=1):
 
-        nods = open(self.nod_file, 'r')
+class qtCell:
+
+    def __init__(self, r, c, qcode, nrow, dxy, xll=0, yll=0):
+
+        # information on parent grid
+        self.nrow = nrow
+        self.dxy = dxy
+        self.xll = xll
+        self.yll = yll
+
+        # information from tree structure file
+        self.r = r
+        self.c = c
+        self.qcode = qcode
+
+        # quadtree refinement information
+        if self.qcode != 0:
+            self.ref = len(self.qcode)
+            self.nsgperbg = 2 ** self.ref
+            self.sgdxy = self.dxy / self.nsgperbg
+
+        # calculate vertices for parent cell
+        self.x0 = self.dxy * self.c + self.xll
+        self.x1 = self.dxy * (self.c - 1) + self.xll
+        self.y0 = self.dxy * (self.nrow - self.r) + self.yll
+        self.y1 = self.dxy * (self.nrow - (self.r - 1)) + self.yll
+
+
+    def rangefinder(self):
+        '''
+        get row / column extent of refined cell within parent cell based on quadtree code
+        '''
+        isgmin = 0
+        isgmax = isgmin + self.nsgperbg
+        jsgmin = 0
+        jsgmax = jsgmin + self.nsgperbg
+
+        # get row column extent of refined cell based on code
+        for q in self.qcode:
+            if q is '1':  #upper left
+                isgmax = isgmin + (isgmax - isgmin) / 2
+                jsgmax = jsgmin + (jsgmax - jsgmin) / 2
+            elif q is '2':  #upper right
+                isgmax = isgmin + (isgmax - isgmin) / 2
+                jsgmin = jsgmax - (jsgmax - jsgmin) / 2
+            elif q is '3':  #lower left
+                isgmin = isgmax - (isgmax - isgmin) / 2
+                jsgmax = jsgmin + (jsgmax - jsgmin) / 2
+            elif q is '4':  #lower right
+                isgmin = isgmax - (isgmax - isgmin) / 2
+                jsgmin = jsgmax - (jsgmax - jsgmin) / 2
+            else:
+                msg = 'Uknown quadrant code: ' + q + ' for cell ' + str(self.r, self.c)
+                raise Exception(msg)
+
+        return isgmin, isgmax, jsgmin, jsgmax
+
+
+    def ts2coords(self):
+        '''
+        get vertices for refined cell using range finder and parent cell vertices
+        '''
+
+        # get the row / column extent of refine qt cell within parent cell
+        isgmin, isgmax, jsgmin, jsgmax = self.rangefinder()
+
+        # now convert refined cell row column extent to cell coordinates
+        ymax, ymin, xmin, xmax = self.sgdxy * np.array([self.nsgperbg - isgmin,
+                                                        self.nsgperbg - isgmax,
+                                                        jsgmin,
+                                                        jsgmax])
+        # return cell coordinates as global coordinates
+        return xmin + self.x0, ymin + self.y0, xmax + self.x0, ymax + self.y0
+
+
+    def get_centroid(self, r, c):
+        '''
+        rcl (tuple) (row, column, layer)
+
+        '''
+        x = self.dxy * (c - 0.5)
+        y = self.dxy * (self.nrow - r + 0.5)
+        return x, y
+
+
+
+class gridSPC(parentGrid):
+
+    def write_from_nodfile(self, nod_file, iz=1, ic=1):
+        '''
+        write spc file from nod file (note: the node numbering is wrong compared to MODFLOW!
+        '''
+        nods = open(nod_file, 'r')
 
         nnods = int(nods.readline().strip().split()[0])
         nvertex = nnods * 8
 
-        ofp = open(self.outfile, 'w')
+        outfile = nod_file[:-4] + '.spc'
+        ofp = open(outfile, 'w')
         ofp.write('UNSTRUCTURED GWF\n')
-        ofp.write('{} {} {} {}\n'.format(nnods, nlay, iz, ic))
+        ofp.write('{} {} {} {}\n'.format(nnods, self.nlay, iz, ic))
         ofp.write('{}\n'.format(nvertex))
 
 
@@ -70,22 +149,24 @@ class gridSPC:
             ymin, ymax = cy - 0.5 * dy, cy + 0.5 * dy
             zmin, zmax = cz - 0.5 * dz, cz + 0.5 * dz
 
-            verts = [(xmin, ymin, zmin),
-                     (xmin, ymax, zmin),
+            verts = [(xmin, ymin, zmax),
+                     (xmax, ymin, zmax),
+                     (xmax, ymax, zmax),
+                     (xmin, ymax, zmax),
+                     (xmin, ymin, zmin),
                      (xmax, ymin, zmin),
                      (xmax, ymax, zmin),
-                     (xmin, ymin, zmax),
-                     (xmin, ymax, zmax),
-                     (xmax, ymin, zmax),
-                     (xmax, ymax, zmax)]
+                     (xmin, ymax, zmin)]
 
             return verts
-
 
         for line in nods:
 
             verts = vertices(line)
             [[ofp.write(' '.join(map(str, v)) + '\n')] for v in verts]
+
+        nods = open(self.nod_file, 'r')
+        nods.next()
 
         i = 1
         inode = 1
@@ -94,35 +175,124 @@ class gridSPC:
             lay = int(line.split()[1])
             x, y, z = map(float, line.split()[2:5])
 
-            ofp.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.0f} 8'.format(inode, x, y, z, lay))
+            ofp.write('{:.0f} {:.2f} {:.2f} {:.2f} {:.0f} 8'.format(inode, x, y, z, lay))
 
-            ofp.write([' {:.0f}'.format(c) for c in np.arange(i, i + 8)])
+            [ofp.write(' {:.0f}'.format(c)) for c in np.arange(i, i + 8)]
             ofp.write('\n')
             i += 8
             inode += 1
 
+        ofp.close()
+
+    def centroid(self, vertxy, vertz):
+        '''
+        return cell centroid from vertices
+        '''
+        xmin, ymin, = vertxy[0]
+        zmax = vertz[0]
+        xmax, ymax = vertxy[6]
+        zmin = vertz[6]
+
+        cx = 0.5 * (xmin + xmax)
+        cy = 0.5 * (ymin + ymax)
+        cz = 0.5 * (zmin + zmax)
+
+        return cx, cy, cz
 
 
-    def write(self):
+    def write_from_tsf(self, tsf_file, iz=1, ic=1, smooth=False, outfile=None):
+        '''
+        write grid spc file from tree structure file
 
-        ofp = open(self.outfile, 'w')
+        '''
+        # total number of cell vertices to be listed
+        self.nvertext = self.nnod * 8
 
+        if not outfile:
+            outfile = tsf_file[:-4] + '.spc'
+
+        print 'reading quadtree grid information from {}...'.format(tsf_file)
+        print 'writing output to {}...'.format(outfile)
+
+        # open output file and write header
+        ofp = open(outfile, 'w')
         ofp.write('UNSTRUCTURED GWF\n')
-        ofp.write('{} {} {} {}\n'.format(self.nnode, self.nlay, self.iz, self.ic))
-        ofp.write('{}\n'.format(self.nvertex))
+        ofp.write('{} {} {} {}\n'.format(self.nnod, self.nlay, iz, ic))
+        ofp.write('{}\n'.format(self.nvertext))
 
-        # write out list of vertices (including duplicates)
-        [ofp.write(' '.join(v) + '\n') for v in self.nods['verts']]
+        # open tsf file
+        nods = open(tsf_file, 'r')
+        nods.next() # skip the header
 
-        # write out node information
-        i = 1
-        for i, r in self.nods.iterrows():
+        allvertxy = np.zeros((self.nnod, 8), dtype=('float,float'))
+        allvertz = np.zeros((self.nnod, 8), dtype=('float'))
+        layer = np.zeros(self.nnod)
+        for line in nods:
 
-            ofp.write('{:.0f} {:.0f} {:.0f} {:.0f} {:.0f} {:.0f}'.format())
+            inode = int(line.split(',')[0])
 
-            ofp.write([' {:.0f}'.format(c) for c in np.arange(i, i + 8)])
-            ofp.write('\n')
-            i += 8
+            # skip inactive nodes
+            if inode == -1:
+                continue
+
+            r, c, l = map(int, line.split()[1].replace('(', '').replace(')', '').split(','))
+            try:
+                qcode = [q for q in line.strip().split()[2]]
+            except:
+                qcode = 0
+
+
+            # initialize class for quadtree cell
+            cell = qtCell(r, c, qcode, self.nrow, self.dxy, xll=self.xll, yll=self.yll)
+
+            if qcode == 0:
+                xmin, ymin, xmax, ymax = cell.x0, cell.y0, cell.x1, cell.y1
+
+            else:
+                # get coordinates of child cell relative to xmin/xmax of parent
+                xmin, ymin, xmax, ymax = cell.ts2coords()
+
+            # get zmin / zmax values
+            zmin, zmax = self.tops[inode - 1], self.bots[inode - 1]
+
+            # make list of cell vertices in ccw order, top then bottom
+            verts = [(xmin, ymin, zmax),
+                     (xmax, ymin, zmax),
+                     (xmax, ymax, zmax),
+                     (xmin, ymax, zmax),
+                     (xmin, ymin, zmin),
+                     (xmax, ymin, zmin),
+                     (xmax, ymax, zmin),
+                     (xmin, ymax, zmin)]
+
+            vertsxy = [v[0:2] for v in verts]
+            for i, v in enumerate(vertsxy):
+                allvertxy[inode - 1, i] = v
+                allvertz[inode - 1, i] = verts[i][2]
+                layer[inode - 1] = l
+
+        if smooth:
+
+            #implement option to remove duplicate vertices here
+            pass
+        else:
+
+            for n in range(len(allvertxy)):
+
+                [ofp.write('{:.2f} {:.2f} {:.2f}\n'.format(allvertxy[n, v][0], allvertxy[n, v][1], allvertz[n, v]))
+                 for v in range(8)]
+                
+            i = 1
+            for n in range(len(allvertxy)):
+
+                x, y, z = self.centroid(allvertxy[n], allvertz[n])
+
+                ofp.write('{:.0f} {:.2f} {:.2f} {:.2f} {:.0f} 8'.format(n+1, x, y, z, layer[n]))
+
+                [ofp.write(' {:.0f}'.format(c)) for c in np.arange(i, i + 8)]
+                ofp.write('\n')
+                i += 8
 
         ofp.close()
+
 
--- write_spc.py
+++ write_spc.py
@@ -1,14 +1,27 @@
 '''
 example script for writing a usg grid spec file
 '''
-
+import os
 import sys
-sys.path.append('/Users/aleaf/Documents/GitHub/Quadtree_utils/')
+sys.path.append('D:ATLData/Documents/GitHub/Quadtree_utils/')
 
-import gridspec
+import gridspec as gs
 
-nod_file = '/Users/aleaf/Documents/ATLData/Wbasin/output_usgdata/grid02qtg.nod'
+tsf_file = 'D:/ATLData/Wbasin/grid02qtg.tsf'
+path = 'D:/ATLData/Wbasin'
+topfiles = [os.path.join(path, f) for f in os.listdir(path) if '.top' in f]
+botfiles = [os.path.join(path, f) for f in os.listdir(path) if '.bot' in f]
+nrow, ncol, nlay = 456, 468, 8
+delrc = 5249.3438
+xll = -1.12795e+006
+yll = 1.71332e+007
 
-spc = gridspec.gridSPC(nod_file)
+# instantiate gridSPC class with parent grid information
+spc = gs.gridSPC(nrow, ncol, nlay, xll=xll, yll=yll, dxy=delrc)
 
-spc.write_byline(8)
+# read in top and bottom elevations for each node
+spc.read_elevations(topfiles, botfiles)
+
+# read tsf and write grid spec file
+spc.write_from_tsf(tsf_file, outfile='D:/ATLData/Wbasin/test.spc')
+
